58a3a82c99718ed31fd239ee3c19b00ab050c459
Brayarn Ogeda <brayarnogeda@gmail.com>
2025-09-04 17:42:12 +0300

    add tab navigation pattern

---
+++ prodev-mobile-app-0x05/app/(home)/inbox.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native'
+const Inbox = () =>{
+    return(
+        <View>
+        <Text>Inbox</Text>
+        </View>
+        )
+    }
+export default Inbox
---
+++ prodev-mobile-app-0x05/app/(home)/saved.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native'
+const Saved = () =>{
+    return(
+        <View>
+        <Text>Saved</Text>
+        </View>
+        )
+    }
+export default Saved
